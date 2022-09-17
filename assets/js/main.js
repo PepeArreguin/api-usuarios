b369f96924ee14e9c23036c9d2910015832a5177
PepeArreguin <arreguinpepe@gmail.com>
2022-09-17 14:02:49 -0500

    get y post ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,11 @@ const tblUsuarios = document.getElementById("tblUsuarios");
 
 function cargarUsuarios () {
 //Es una promesa
-fetch(base_url_api + "/Main/alumnos")
+fetch(base_url_api + "/Main/alumnos",
+    {
+        method: "GET"
+    }
+)
   .then(response => response.json())
   .then(result => {
     console.log(result);
@@ -13,13 +17,16 @@ fetch(base_url_api + "/Main/alumnos")
     //hacer un ciclo para que pinte en la tabla, es mas comodo hacer un for of
     for (const usuario of result.data) {
         let tr = `<tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td>4</td>
-        <td>5</td>
+        <td>${usuario.id}</td>
+        <td>${usuario.nombre}</td>
+        <td>${usuario.paterno}</td>
+        <td>${usuario.materno}</td>
+        <td>${usuario.email}</td>
         </tr>`;
         tblUsuarios.innerHTML += tr;
+    }
+    if (result.data.length == 0){
+        tblUsuarios.innerHTML = `<tr><td colspan = "5" class= "text-center">No Hay usuarios</td></tr>`
     }
 
 })
@@ -33,6 +40,39 @@ fetch(base_url_api + "/Main/alumnos")
 
 function agregarUsuario(){
     console.log("entro a agregar user");
-}
+    let form_data = new FormData();
+    form_data.append("nombre", document.getElementById("nombre").value);
+    form_data.append("paterno", document.getElementById("paterno").value);
+    form_data.append("materno", document.getElementById("materno").value);
+    form_data.append("email", document.getElementById("email").value);
+    
 
-cargarUsuarios();
+    fetch(base_url_api + "/Main/alumnos",
+        {
+            method: "POST",
+            body: form_data
+        }
+    )
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        limpiarFormulario();
+        cargarUsuarios();
+    })
+    .catch((error) => {
+        console.log("Error detectado");
+        })
+
+
+    }
+
+
+    function limpiarFormulario(){
+        //Limpiar el formulario
+        document.getElementById("nombre").value = "";
+        document.getElementById("paterno").value = "";
+        document.getElementById("materno").value = "";
+        document.getElementById("email").value = "";
+    }
+
+    cargarUsuarios();
